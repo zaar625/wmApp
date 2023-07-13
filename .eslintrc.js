e0a6d7ad0d0ad0f353dd0b5f798df653053a5dac
이상윤 <zaar625@gmail.com>
2023-07-13 17:48:40 +0900

    eslint setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,21 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'prettier/prettier': [
+      {
+        requirePragma: true,
+        singleQuote: true,
+        trailingComma: 'all',
+        bracketSpacing: false,
+        jsxBracketSameLine: true,
+        parser: 'flow',
+        semi: true
+      },
+    ],
+    // 'react/no-unstable-nested-components': ['error', { allowAsProps: true }],
+    // quotes: 0,
+    // "react-native/no-inline-styles": ["error", { "ignorePattern": "something" }],
+    // '@typescript-eslint/no-unused-vars': 'off',
+  },
 };
