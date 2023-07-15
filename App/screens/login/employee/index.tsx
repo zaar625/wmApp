67ec41c5364edb09cad5e3964c01f83450d42dac
이상윤 <zaar625@gmail.com>
2023-07-15 15:01:37 +0900

    Loginpage published

--- App/screens/login/employee/index.tsx
+++ App/screens/login/employee/index.tsx
@@ -4,7 +4,8 @@ import {
   View,
   TouchableWithoutFeedback,
   Keyboard,
-  TextInput
+  TextInput,
+  Pressable
 } from 'react-native';
 import React, { useRef } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -27,6 +28,18 @@ export default function EmployeeLoginPage() {
         {/* 인풋박스 */}
         <InputBox fn={handleInputBox01Submit} />
         <InputBox ref={inputBoxRef2} />
+        {/* 버튼 */}
+        <Pressable style={({ pressed }) => [styles.Btn, { opacity: pressed ? 0.6 : 1 }]}>
+          <Text style={styles.btnText}>로그인</Text>
+        </Pressable>
+        {/* 회원가입 및 비밀번호 */}
+        <View style={styles.subBtn}>
+          <Text style={[styles.subBtnText]}>회원가입</Text>
+          <View
+            style={{ height: '60%', backgroundColor: '#C9C9C9', width: 1, marginHorizontal: 10 }}
+          />
+          <Text style={styles.subBtnText}>비밀번호찾기</Text>
+        </View>
       </SafeAreaView>
     </TouchableWithoutFeedback>
   );
@@ -44,5 +57,26 @@ const styles = StyleSheet.create({
     fontWeight: '700',
     lineHeight: 36,
     marginBottom: 40
+  },
+  Btn: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 15,
+    backgroundColor: '#326273',
+    borderRadius: 10
+  },
+  btnText: {
+    fontSize: 14,
+    fontWeight: '500',
+    color: '#fff'
+  },
+  subBtn: {
+    flexDirection: 'row',
+    alignSelf: 'center',
+    alignItems: 'center',
+    marginTop: 20
+  },
+  subBtnText: {
+    color: '#fff'
   }
 });
