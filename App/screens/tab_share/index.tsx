246e885c9b774b2a62625f03be1870ad57bb8951
이상윤 <zaar625@gmail.com>
2023-07-24 23:33:58 +0900

    ios_pakage.json fixed_ qr lib is depandency permission lib 2.x to 3.x

--- App/screens/tab_share/index.tsx
+++ App/screens/tab_share/index.tsx
@@ -1,10 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import QRCodeScanner from 'react-native-qrcode-scanner';
 
 const ShareTabScreen = () => {
+  const onSuccess = async (data: any) => {
+    console.log(data);
+  };
   return (
     <View>
-      <Text>ShareTabScreen</Text>
+      <QRCodeScanner
+        onRead={onSuccess}
+        showMarker
+        cameraStyle={{ height: Dimensions.get('window').height }}
+      />
     </View>
   );
 };
