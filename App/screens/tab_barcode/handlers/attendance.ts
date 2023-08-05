a06a5c44fc68d4ca796d52fc5b56bd4a198a31b0
이상윤 <zaar625@gmail.com>
2023-08-06 00:23:18 +0900

    출퇴근 핸들러 함수 테스트입니다. 로직은 완료되었으나, 리팩토링이 필요합니다.

---
+++ App/screens/tab_barcode/handlers/attendance.ts
@@ -0,0 +1,52 @@
+import firestore from '@react-native-firebase/firestore';
+
+const workHourCollection = firestore()
+  .collection('users')
+  .doc('DMWrTCluLrhJMrI01BVhJK6byFs1')
+  .collection('workHour');
+
+export const onAttendance = async (currentDate: any) => {
+  const code = 'ItGwR9Tj8BNlgxR37Nrp';
+  const workHourRef = workHourCollection.doc(currentDate);
+
+  const workData = await workHourRef.get();
+
+  const workArray = await workData.data()?.work; // undefined || [item]
+
+  if (workArray) {
+    const updateWorkArray = workArray.map((work: any) => {
+      if (work.storeName === code) {
+        return {
+          ...work,
+          end: new Date()
+        };
+      }
+      return work;
+    });
+
+    const isNewStore = workArray.some((work: any) => work.storeName === code);
+    console.log(isNewStore);
+
+    if (!isNewStore) {
+      workHourCollection.doc(currentDate).update({
+        work: firestore.FieldValue.arrayUnion({
+          date: new Date(),
+          end: null,
+          start: new Date(),
+          storeName: code
+        })
+      });
+    } else {
+      workHourCollection.doc(currentDate).update({ work: updateWorkArray });
+    }
+  } else {
+    workHourCollection.doc(currentDate).set({
+      work: firestore.FieldValue.arrayUnion({
+        date: new Date(),
+        end: null,
+        start: new Date(),
+        storeName: code
+      })
+    });
+  }
+};
--- App/screens/tab_barcode/index.tsx
+++ App/screens/tab_barcode/index.tsx
@@ -1,61 +1,43 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-import QRCodeScanner from 'react-native-qrcode-scanner';
 import { BarCodeReadEvent } from 'react-native-camera';
-
-import { deviceWidth, deviceheight } from '../../theme';
 import themeChange from '../../util/theme';
-
 import firestore from '@react-native-firebase/firestore';
+import format from 'date-fns/format';
 
-const workHourRef = firestore()
-  .collection('users')
-  .doc('DMWrTCluLrhJMrI01BVhJK6byFs1')
-  .collection('workHour');
+import { onAttendance } from './handlers/attendance';
 
 const BarcodeTabScreen = () => {
+  const workHourRef = firestore()
+    .collection('users')
+    .doc('DMWrTCluLrhJMrI01BVhJK6byFs1')
+    .collection('workHour');
+
   const themeMode = themeChange();
+  const currentTime = new Date();
+  const currentDate = format(currentTime, 'yyyy-MM');
 
-  const scanerHandler = async (event: BarCodeReadEvent) => {
-    const storeId = event.data;
-    const currentTime = new Date();
-    console.log(currentTime);
-
-    console.log((await workHourRef.doc(String(currentTime)).get()).exists);
-
-    workHourRef
-      .doc(String(currentTime))
-      .set({ work: [{ start: currentTime, end: null, storeName: storeId, date: currentTime }] });
-
-    // const { work }: any = dateWork;
-
-    // dateWork.update({
-    //   work: [...work, { start: currentTime, end: null, storeName: storeId, date: currentTime }]
-    // });
-  };
+  // console.log(workHourRef.doc(String(currentTime)).get().exists);
 
   return (
-    <View style={{ flex: 1, backgroundColor: themeMode.primary }}>
-      {/* <QRCodeScanner
-        onRead={scanerHandler}
-        showMarker={true}
-        cameraStyle={{
-          width: deviceWidth,
-          height: 300,
-          alignSelf: 'center'
-        }}
-        topContent={
-          <View>
-            <Text style={{ color: '#fff' }}>출퇴근</Text>
-          </View>
-        }
-        reactivate={true}
-        reactivateTimeout={5000}
-      /> */}
+    <View style={[styles.container, { backgroundColor: themeMode.primary }]}>
+      <Pressable onPress={() => onAttendance(currentDate)}>
+        <Text style={{ color: '#FFF' }}>출근</Text>
+      </Pressable>
+      <Pressable onPress={onAttendance}>
+        <Text style={{ color: '#FFF' }}>퇴근</Text>
+      </Pressable>
     </View>
   );
 };
 
 export default BarcodeTabScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  }
+});
