6a999713c25f13094f14cc7caddc4eba505139d5
이상윤 <zaar625@gmail.com>
2023-08-18 11:50:34 +0900

    loading animation added

--- App/common-components/bottom_sheet/Loader.tsx
+++ App/common-components/bottom_sheet/Loader.tsx
@@ -1,6 +1,8 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import { useIsFetching, useIsMutating } from '@tanstack/react-query';
+import LottieView from 'lottie-react-native';
+import { deviceWidth, deviceheight } from '../../theme';
 
 const Loader = () => {
   const isFetching = useIsFetching();
@@ -9,9 +11,14 @@ const Loader = () => {
   console.log('isFetching', isFetching);
   return (
     <>
-      {isFetching > 0 && (
+      {isFetching === 0 && (
         <View style={[styles.container, StyleSheet.absoluteFillObject]}>
-          <Text style={{ color: '#fff' }}>Loader</Text>
+          <LottieView
+            source={require('../../assets/loader.json')}
+            autoPlay
+            loop
+            style={{ width: deviceWidth * 0.5, height: deviceheight }}
+          />
         </View>
       )}
     </>
