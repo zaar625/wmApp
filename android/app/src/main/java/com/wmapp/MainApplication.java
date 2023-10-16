79187eacdc75312013374da43ab7ac68c4a496b1
이상윤 <zaar625@gmail.com>
2023-10-16 16:05:32 +0900

    android code push added

--- android/app/src/main/java/com/wmapp/MainApplication.java
+++ android/app/src/main/java/com/wmapp/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 import java.util.List;
 
+import com.microsoft.codepush.react.CodePush;
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
@@ -31,6 +32,11 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected String getJSMainModuleName() {
           return "index";
+        }
+
+        @Override
+        protected String getJSBundleFile() {
+            return CodePush.getJSBundleFile();
         }
 
         @Override
